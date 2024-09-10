a502b269d5ff6010652097b1dbe3e3873d3aebba
tannerdykstra22 <tannerdykstra22@gmail.com>
2024-09-10 16:37:55 -0500

    center imgs horizontally and captions underneath them in languages.html

--- main.js
+++ main.js
@@ -7,6 +7,17 @@
 //     return alert ("Portfolio still in progress - Come back soon!");
 // }
 
+// CHECK THIS BELOW FUNCTION OUT!!
+// function addRecommendation() {
+//     // Get the message of the new recommendation
+//     let recommendation = document.getElementById("new_recommendation");
+//     // If the user has left a recommendation, display a pop-up
+//     if (recommendation.value != null && recommendation.value.trim() != "") {
+//       console.log("New recommendation added");
+//       //Call showPopup here
+//       showPopup(true);
+  
+
 const portfolioUnfinishedAlert = document.getElementsById("portfolio-nav")
 
 portfolioUnfinishedAlert.onclick = function (event) {
