c040831ba60badf6bc290c01c915a46790f17804
tannerdykstra22 <tannerdykstra22@gmail.com>
2024-09-10 17:46:06 -0500

    fix links in all html pages to channel an alert on unfinished pages (portfolio and references)

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
+function showPortfolioAlert() {
+    var myText= "No Projects Yet - Please Revisit Soon";
+    alert(myText);
+}
 
-// if (onclick[Element] === 'portfolio.html') {
-//     alert ("Portfolio still in progress - Come back soon!");
-// }
-
-// if (onclick.Element === 'portfolio.html') {
-//     return alert ("Portfolio still in progress - Come back soon!");
-// }
-
-// CHECK THIS BELOW FUNCTION OUT!!
-// function addRecommendation() {
-//     // Get the message of the new recommendation
-//     let recommendation = document.getElementById("new_recommendation");
-//     // If the user has left a recommendation, display a pop-up
-//     if (recommendation.value != null && recommendation.value.trim() != "") {
-//       console.log("New recommendation added");
-//       //Call showPopup here
-//       showPopup(true);
-  
-
-const portfolioUnfinishedAlert = document.getElementsById("portfolio-nav")
-
-portfolioUnfinishedAlert.onclick = function (event) {
-    event.preventDefault();
-    alert("Portfolio still in progress - Come back soon!");
+function showReferencesAlert() {
+    var myText= "No References Yet - Please Revisit Soon";
+    alert(myText);
 }
